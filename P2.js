c3ccdfec6715ae8684b94b919146ed1640dca7a1
Abhinav Pandey <abhinavpandey56393@gmail.com>
2024-04-15 23:20:19 +0530

    add new program

--- P2.js
+++ P2.js
@@ -1,1 +1,13 @@
-let row = [];
+// 1
+// 12
+// 123
+// 1234
+// 12345
+let num =5;
+for (let i = 0;i<=num;i++){
+    let row = '';
+    for (let j = 1;j<=i;j++){
+        row += j;
+    }
+    console.log(row);
+}
